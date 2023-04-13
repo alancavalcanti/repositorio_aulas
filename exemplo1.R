03b64ea53595d429232cd7adba87d45f76506c77
Alan Cavalcanti <alan.cavalcanti@ufpe.br>
2023-04-13 15:19:36 -0300

    exemplo de dataframe com nome e datas de alunos das turmas criado para aula de mestrado

--- exemplo1.R
+++ exemplo1.R
@@ -1,1 +1,28 @@
 summary(iris)
+
+
+#criando data frame
+
+#primeiro, vamos instalar um novo pacote: eeptools
+install.packages('eeptools')
+
+#depois, vamos chamar o pacote
+library(eeptools)
+
+# vetor com nome dos alunos
+nomeAluno <- c("Alan", "Rayanne", "Rennan", "Renata", "Eveline", "Isabela")
+
+
+# vetor com datas de nascimento
+nascimentoAluno <- as.Date(c("2000-07-28", "1992-03-21", "1993-07-20", "1989-07-20", "1994-01-25", "1985-12-15"))
+
+# vetor com as idades
+idadeAluno <- round( age_calc( nascimentoAluno, units = 'years')) # Idade usando a função age_calc do pacote eeptools e a função round (arredondar)
+
+# data.frame com base nos vetores
+listaAlunos <- data.frame(
+  nome = nomeAluno,      # Nome 
+  dataNascimento = nascimentoAluno, # Data de nascimento
+  idade = idadeAluno) # idade
+
+save(listaAlunos, file = "exemplo_listaAlunos.Rda")  
