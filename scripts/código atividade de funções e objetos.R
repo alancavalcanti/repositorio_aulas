83ff7a453a828acb5a18a4958498473b45ad0c66
Alan Cavalcanti <alan.cavalcanti@ufpe.br>
2023-04-18 16:31:38 -0300

    código atividade funções e objetos corrigidos

--- scripts/código atividade de funções e objetos.R
+++ scripts/código atividade de funções e objetos.R
@@ -8,3 +8,4 @@ str(vetorExemplo) #mostrando a complexidade do vetor
 #criando um objeto através de uma função de regressão. Usei uma base de dados específica e uma variável dela
 
 regExemplo <- lm(dispersive_nm_IDC ~ ., x)
+str(regExemplo) #mostrando a complexidade do objeto criado pela função de regressão
