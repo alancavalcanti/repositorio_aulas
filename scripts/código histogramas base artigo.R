f008f163f56f455c04e59b00d907b7d4cda30d9a
Alan Cavalcanti <alan.cavalcanti@ufpe.br>
2023-04-18 16:13:30 -0300

    base de dados do Strom e Script da atividade de centralização

--- scripts/código histogramas base artigo.R
+++ scripts/código histogramas base artigo.R
@@ -1,2 +1,12 @@
 #utilizando função apply para construir histograma da base de dados de um artigo que estou escrevendo
 sapply(base_artigo[ , 16:18], hist)
+
+#fazendo centralização de uma variável de uma base de dados
+
+hist(x$dispersive_nm_IDC)
+
+simulacaoDispersive <- x$dispersive_nm_IDC
+hist(simulacaoDispersive)
+
+SimulacaoDispersiveCentral <- simulacaoDispersive - mean(simulacaoDispersive)
+hist(SimulacaoDispersiveCentral)
