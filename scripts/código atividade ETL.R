7a991c14a6c7ed8fab24668e15d285662203a667
Alan Cavalcanti <alan.cavalcanti@ufpe.br>
2023-04-30 00:11:06 -0300

    atividade ETL codigo corrigido

--- scripts/código atividade ETL.R
+++ scripts/código atividade ETL.R
@@ -1,5 +1,5 @@
 library(tidyverse)
-
+library(dplyr)
 pacman::p_load(dplyr)
 
 sinistrosRecife2019Raw <- read.csv2('http://dados.recife.pe.gov.br/dataset/44087d2d-73b5-4ab3-9bd8-78da7436eed1/resource/3531bafe-d47d-415e-b154-a881081ac76c/download/acidentes-2019.csv', sep = ';', encoding = 'UTF-8')
@@ -9,13 +9,10 @@ sinistrosRecife2020Raw <- read.csv2('http://dados.recife.pe.gov.br/dataset/44087
 sinistrosRecife2021Raw <- read.csv2('http://dados.recife.pe.gov.br/dataset/44087d2d-73b5-4ab3-9bd8-78da7436eed1/resource/2caa8f41-ccd9-4ea5-906d-f66017d6e107/download/acidentes_2021-jan.csv', sep = ';', encoding = 'UTF-8')
 
 # junta as bases de dados com comando rbind (juntas por linhas)
-colunas_iguais <- names(sinistrosRecife2020Raw[
-  intersect(
-    names(sinistrosRecife2020Raw), names(sinistrosRecife2021Raw))])
+sinistrosRecife2021Raw$num_semaforo <- as.integer(sinistrosRecife2021Raw$num_semaforo)
 
-sinistrosRecife2020Raw <- sinistrosRecife2020Raw %>% select(all_of(colunas_iguais))
 
-sinistrosRecifeRaw <- rbind(sinistrosRecife2020Raw, sinistrosRecife2021Raw)
+sinistrosRecifeRaw <- bind_rows(sinistrosRecife2019Raw, sinistrosRecife2020Raw, sinistrosRecife2021Raw)    
 
 # observa a estrutura dos dados
 str(sinistrosRecifeRaw)
