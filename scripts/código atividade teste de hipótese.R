4fb4d85f744dff93c219c2121209c9eefae217bc
Alan Cavalcanti <alan.cavalcanti@ufpe.br>
2023-06-12 23:25:59 -0300

    codigo atualizado

--- scripts/código atividade teste de hipótese.R
+++ scripts/código atividade teste de hipótese.R
@@ -2,3 +2,5 @@ pacman::p_load(data.table)
 
 
 cor.test(base_artigo_final_v2$positive_e_ios, base_artigo_final_v2$positive_c1_ios)
+cor.test(base_artigo_final_v2$positive_e_ios, base_artigo_final_v2$negative_c2_ios)
+cor.test(base_artigo_final_v2$v_positive_e_ios, base_artigo_final_v2$v_positive_c1_ios)
